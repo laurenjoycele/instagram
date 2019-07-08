136a24b66004fc988532a6096592fe1e8dd6f2ed
Lauren Le <laurenjle@fb.com>
2019-07-08 15:36:51 -0700

    Initial Commit

---
+++ instagram-lauren/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instagram-lauren
+//
+//  Created by laurenjle on 7/8/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
